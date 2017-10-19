2a5766128a85f9f09dbbcb239045a120cc6f51ce
Glitch (starter-react-router) <none>
2017-10-19 19:12:44 +0000

    Checkpoint

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const BrowserRouter = require('react-router-dom').BrowserRouter;
 
 /* Import Components */
 const HelloWorld = require('./components/HelloWorld');
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
+ReactDOM.render(<BrowserRouter><HelloWorld/></BrowserRouter>, document.getElementById('main'));
