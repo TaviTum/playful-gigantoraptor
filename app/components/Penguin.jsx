5bee1a7eac0739eb4d200cf179364f75c6ccbd4f
Glitch (starter-react-router) <none>
2019-10-04 14:38:38 +0000

    🏋💎 Checkpoint ./.glitch-assets:6278369/448 ./app/components/Penguin.jsx:6278369/412 ./static/style.css:6278369/63 ./app/index.html:6278369/169

--- app/components/Penguin.jsx
+++ app/components/Penguin.jsx
@@ -6,9 +6,9 @@ const Penguin = function() {
   return (
     <div className="penguin">
       <h1>Qui-qui-qui!</h1>
-      <img src=""/>
 
       <p>This is a penguin!</p>
+      <img src="https://cdn.glitch.com/83b3f022-25b2-442f-9ea6-9c382b3f2d5e%2F%D0%BF%D0%B8%D0%BD%D0%B3%D0%B2%D0%B8%D0%BD.png?v=1570199284922"/>
 
       <div className="link-container">
         <Link to='/main'>Go home</Link>
